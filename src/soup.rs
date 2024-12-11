10b0313efcfe5896faa99bc212f54bdc80925c47
agentelement <agentelement@protonmail.com>
2024-12-11 14:37:21 -0700

    refactor: make soup independent of lambda calculus

--- src/soup.rs
+++ src/soup.rs
@@ -1,206 +1,78 @@
-use core::fmt;
-use std::fmt::{Debug, Display};
+use std::{
+    fmt::{Debug, Display},
+    marker::PhantomData,
+};
 
-use crate::config;
-use lambda_calculus::{abs, app, Term, Var};
-use rand::{Rng, SeedableRng};
+use crate::collidable::{Collider, Particle, Residue};
+
+use rand::Rng;
 use rand_chacha::ChaCha8Rng;
 
 /// The principal AlChemy object. The `Soup` struct contains a set of
 /// lambda expressions, and rules for composing and filtering them.
 #[derive(Debug, Clone)]
-pub struct Soup {
-    expressions: Vec<Term>,
-    n_collisions: usize,
-    reaction_rules: Vec<Term>,
-    reduction_limit: usize,
-    size_limit: usize,
+pub struct Soup<P, C, T, E> {
+    // All of these pub(crate)s here are hacky
+    pub(crate) expressions: Vec<P>,
+    pub(crate) n_collisions: usize,
+    pub(crate) collider: C,
+    pub(crate) reduction_limit: usize,
+    pub(crate) size_limit: usize,
 
-    maintain_constant_population_size: bool,
-    discard_copy_actions: bool,
-    discard_identity: bool,
-    discard_free_variable_expressions: bool,
-    discard_parents: bool,
+    pub(crate) maintain_constant_population_size: bool,
+    pub(crate) discard_parents: bool,
 
-    rng: ChaCha8Rng,
+    pub(crate) rng: ChaCha8Rng,
+
+    // TODO: Figure out how to get rid of these horrible phantomdatas
+    pub(crate) t: PhantomData<T>,
+    pub(crate) e: PhantomData<E>,
 }
 
-pub struct Tape {
-    soup: Soup,
-    history: Vec<Soup>,
+pub struct Tape<P, C, T, E> {
+    soup: Soup<P, C, T, E>,
+    history: Vec<Soup<P, C, T, E>>,
     polling_interval: usize,
 }
 
-/// Stores the size and number of reductions for a collision
-struct CollisionResult {
-    pub size: u32,
-    pub reductions: usize,
-}
-
-/// The result of composing a vector `v` of 2-ary lambda expressions with
-/// the expressions A and B.
-struct ReactionResult {
-    pub collision_results: Vec<CollisionResult>,
-
-    /// Size of A
-    pub left_size: u32,
-
-    /// Size of B
-    pub right_size: u32,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum ReactionError {
-    ExceedsReductionLimit,
-    NotEnoughExpressions,
-    IsIdentity,
-    IsParent,
-    HasFreeVariables,
-    ExceedsDepthLimit,
-}
-
-pub fn reduce_with_limit(
-    expr: &mut Term,
-    rlimit: usize,
-    slimit: usize,
-) -> Result<usize, ReactionError> {
-    let mut n = 0;
-    for _ in 0..rlimit {
-        if expr.reduce(lambda_calculus::HAP, 1) == 0 {
-            break;
-        }
-
-        // WARNING: This is EXTREMELY expensive. Calling max_depth is log(depth), and is done
-        // per reduction step. Remove when possible.
-        let depth = expr.size();
-        if depth > slimit {
-            return Err(ReactionError::ExceedsDepthLimit);
-        }
-        n += 1;
-    }
-    Ok(n)
-}
-
-impl Soup {
-    /// Generate an empty soup with the following configuration options:
-    pub fn new() -> Self {
-        Soup::from_config(&config::Reactor::new())
-    }
-
-    /// Generate an empty soup from a given `config` object.
-    pub fn from_config(cfg: &config::Reactor) -> Self {
-        let seed = cfg.seed.get();
-        let rng = ChaCha8Rng::from_seed(seed);
-        Soup {
-            expressions: Vec::new(),
-            reaction_rules: cfg
-                .rules
-                .iter()
-                .map(|r| lambda_calculus::parse(r, lambda_calculus::Classic).unwrap())
-                .collect(),
-            reduction_limit: cfg.reduction_cutoff,
-            size_limit: cfg.size_cutoff,
-
-            maintain_constant_population_size: cfg.maintain_constant_population_size,
-            discard_copy_actions: cfg.discard_copy_actions,
-            discard_parents: cfg.discard_parents,
-            discard_identity: cfg.discard_identity,
-            discard_free_variable_expressions: cfg.discard_free_variable_expressions,
-            rng,
-            n_collisions: 0,
-        }
-    }
-
-    /// Set the reduction limit of the soup
-    pub fn set_limit(&mut self, limit: usize) {
-        self.reduction_limit = limit;
-    }
-
+impl<P, C, T, E> Soup<P, C, T, E>
+where
+    P: Particle + Display + Clone,
+    C: Collider<P, T, E> + Clone,
+    T: Display + Clone + Residue<P>,
+    E: Display + Clone + std::error::Error,
+{
     /// Introduce all expressions in `expressions` into the soup, without
     /// reduction.
-    pub fn perturb(&mut self, expressions: impl IntoIterator<Item = Term>) {
-        self.expressions
-            .extend(expressions.into_iter().filter(|e| !e.has_free_variables()));
-    }
-
-    /// Return the result of ((`rule` `left`) `right`), up to a limit of
-    /// `self.reduction_limit`.
-    // TODO: return a proper error type instead of `String`.
-    fn collide(&self, rule: Term, left: Term, right: Term) -> Result<(Term, usize), ReactionError> {
-        let mut expr = app!(rule, left.clone(), right.clone());
-        let n = reduce_with_limit(&mut expr, self.reduction_limit, self.size_limit)?;
-
-        if n == self.reduction_limit {
-            return Err(ReactionError::ExceedsReductionLimit);
-        }
-
-        let identity = abs(Var(1));
-        if expr.is_isomorphic_to(&identity) && self.discard_identity {
-            return Err(ReactionError::IsIdentity);
-        }
-
-        let is_copy_action = expr.is_isomorphic_to(&left) || expr.is_isomorphic_to(&right);
-        if is_copy_action && self.discard_copy_actions {
-            return Err(ReactionError::IsParent);
-        }
-
-        if expr.has_free_variables() && self.discard_free_variable_expressions {
-            return Err(ReactionError::HasFreeVariables);
-        }
-
-
-        Ok((expr, n))
+    pub fn perturb(&mut self, expressions: impl IntoIterator<Item = P>) {
+        self.expressions.extend(expressions)
     }
 
     /// Produce one atomic reaction on the soup.
-    fn react(&mut self) -> Result<ReactionResult, ReactionError> {
+    pub fn react(&mut self) -> Result<T, E> {
         let n_expr = self.expressions.len();
-
-        if n_expr < 2 {
-            return Err(ReactionError::NotEnoughExpressions);
-        }
 
         // Remove two distinct expressions randomly from the soup
         let i = self.rng.gen_range(0..n_expr);
         let left = &self.expressions.swap_remove(i);
-        let left_size = left.max_depth();
 
         let j = self.rng.gen_range(0..n_expr - 1);
         let right = &self.expressions.swap_remove(j);
-        let right_size = right.max_depth();
 
-        // Record collision information
-        let mut buf = Vec::with_capacity(self.reaction_rules.len());
-        let mut collision_results = Vec::with_capacity(self.reaction_rules.len());
+        // Add collision results to soup
+        let result = self.collider.collide(left.clone(), right.clone());
 
-        // Collide expressions
-        //
-        let mut n_successful_reactions = 0;
-        for rule in &self.reaction_rules {
-            let result = self.collide(rule.clone(), left.clone(), right.clone());
-            self.n_collisions += 1;
-            match result {
-                Ok((value, n)) => {
-                    let datum = CollisionResult {
-                        reductions: n,
-                        size: value.max_depth(),
-                    };
-                    collision_results.push(datum);
-                    buf.push(value);
-                    n_successful_reactions += 1;
-                }
-                Err(s) => {
-                    if !self.discard_parents {
-                        self.expressions.push(left.clone());
-                        self.expressions.push(right.clone());
-                    }
-                    return Err(s);
+        if let Ok(ref t) = result {
+            self.perturb(t.particles());
+
+            // Remove additional expressions, if required.
+            if self.maintain_constant_population_size {
+                for _ in 0..t.count() {
+                    let k = self.rng.gen_range(0..self.expressions.len());
+                    self.expressions.swap_remove(k);
                 }
             }
         }
-
-        // Add collision results to soup
-        self.expressions.append(&mut buf);
 
         // Add removed parents back into the soup, if necessary
         if !self.discard_parents {
@@ -208,38 +80,12 @@ impl Soup {
             self.expressions.push(right.clone());
         }
 
-        // Remove additional expressions, if required.
-        if self.maintain_constant_population_size {
-            for _ in 0..n_successful_reactions {
-                let k = self.rng.gen_range(0..self.expressions.len());
-                self.expressions.swap_remove(k);
-            }
-        }
-
-        // Return collision log
-        Ok(ReactionResult {
-            collision_results,
-            left_size,
-            right_size,
-        })
+        result.clone()
     }
 
-    fn log_failure_reaction(reaction: &Result<ReactionResult, ReactionError>) {
-        if let Err(message) = reaction {
-            println!("failed because {}", message)
-        }
-    }
-
-    fn log_message_from_reaction(reaction: &Result<ReactionResult, ReactionError>) -> String {
+    fn log_message_from_reaction(reaction: &Result<T, E>) -> String {
         match reaction {
-            Ok(result) => format!(
-                "successful with {} reductions between expressions of
-                        sizes {} and {}, and produces an expression of size {}",
-                result.left_size,
-                result.right_size,
-                result.collision_results[0].reductions,
-                result.collision_results[0].size
-            ),
+            Ok(result) => format!("successful with {}", result),
             Err(message) => format!("failed because {}", message),
         }
     }
@@ -249,56 +95,55 @@ impl Soup {
     /// (the fraction of failed reactions).
     pub fn simulate_for(&mut self, n: usize, log: bool) -> usize {
         let mut n_successes = 0;
-        for _ in 0..n {
+        for i in 0..n {
             let reaction = self.react();
             if reaction.is_ok() {
                 n_successes += 1;
             }
 
             if log {
-                // let message = Soup::log_message_from_reaction(&reaction);
-                // println!("reaction {:?} {}", i, message)
-                Soup::log_failure_reaction(&reaction);
+                let message = Self::log_message_from_reaction(&reaction);
+                println!("reaction {:?} {}", i, message)
             }
         }
         n_successes
     }
 
-    pub fn simulate_and_poll<F, T>(
+    pub fn simulate_and_poll<F, R>(
         &mut self,
         n: usize,
         polling_interval: usize,
         log: bool,
         poller: F,
-    ) -> Vec<T>
+    ) -> Vec<R>
     where
-        F: Fn(&Self) -> T,
+        F: Fn(&Self) -> R,
     {
-        let mut data: Vec<T> = Vec::new();
+        let mut data: Vec<R> = Vec::new();
         for i in 0..n {
             let reaction = self.react();
             if (i % polling_interval) == 0 {
                 data.push(poller(self))
             }
             if log {
-                let message = Soup::log_message_from_reaction(&reaction);
+                let message = Self::log_message_from_reaction(&reaction);
                 println!("reaction {:?} {}", i, message)
             }
         }
         data
     }
 
-    pub fn simulate_and_poll_with_killer<F, T>(
+    pub fn simulate_and_poll_with_killer<F, R>(
         &mut self,
         n: usize,
         polling_interval: usize,
         log: bool,
         killpoller: F,
-    ) -> Vec<T>
+    ) -> Vec<R>
     where
-        F: Fn(&Self) -> (T, bool),
+        F: Fn(&Self) -> (R, bool),
     {
-        let mut data: Vec<T> = Vec::new();
+        let mut data: Vec<R> = Vec::new();
         for i in 0..n {
             let reaction = self.react();
             if (i % polling_interval) == 0 {
@@ -309,7 +154,7 @@ impl Soup {
                 };
             }
             if log {
-                let message = Soup::log_message_from_reaction(&reaction);
+                let message = Self::log_message_from_reaction(&reaction);
                 println!("reaction {:?} {}", i, message)
             }
         }
@@ -319,20 +164,25 @@ impl Soup {
     /// Simulate the soup for `n` collisions, recording the state of the soup every
     /// `polling_interval` reactions. If `log` is set, then print out a log message for each
     /// reaction
-    pub fn simulate_and_record(&mut self, n: usize, polling_interval: usize, log: bool) -> Tape {
-        let mut history: Vec<Soup> = Vec::new();
+    pub fn simulate_and_record(
+        &mut self,
+        n: usize,
+        polling_interval: usize,
+        log: bool,
+    ) -> Tape<P, C, T, E> {
+        let mut history: Vec<Self> = Vec::new();
         for i in 0..n {
             let reaction = self.react();
             if (i % polling_interval) == 0 {
                 history.push(self.clone())
             }
             if log {
-                let message = Soup::log_message_from_reaction(&reaction);
+                let message = Self::log_message_from_reaction(&reaction);
                 println!("reaction {:?} {}", i, message)
             }
         }
 
-        Tape {
+        Tape::<P, C, T, E> {
             soup: self.clone(),
             history,
             polling_interval,
@@ -340,8 +190,6 @@ impl Soup {
     }
 
     /// Print out all expressions within the soup. Defaults to Church notation.
-    /// If `debruijn_output` is set, then expressions are printed in DeBruijn
-    /// notation.
     pub fn print(&self) {
         for expression in &self.expressions {
             println!("{}", expression)
@@ -349,7 +197,7 @@ impl Soup {
     }
 
     /// Get an iterator over all expressions.
-    pub fn expressions(&self) -> impl Iterator<Item = &Term> {
+    pub fn expressions(&self) -> impl Iterator<Item = &P> {
         self.expressions.iter()
     }
 
@@ -363,12 +211,18 @@ impl Soup {
     }
 }
 
-impl Tape {
-    pub fn final_state(&self) -> &Soup {
+impl<P, C, T, E> Tape<P, C, T, E>
+where
+    P: Particle + Display + Clone,
+    C: Collider<P, T, E> + Clone,
+    T: Display + Clone + Residue<P>,
+    E: Display + Clone + std::error::Error,
+{
+    pub fn final_state(&self) -> &Soup<P, C, T, E> {
         &self.soup
     }
 
-    pub fn history(&self) -> impl Iterator<Item = &Soup> {
+    pub fn history(&self) -> impl Iterator<Item = &Soup<P, C, T, E>> {
         self.history.iter()
     }
 
@@ -376,34 +230,3 @@ impl Tape {
         self.polling_interval
     }
 }
-
-/// Clippy asked me to do this
-impl Default for Soup {
-    fn default() -> Self {
-        Soup::new()
-    }
-}
-
-impl fmt::Display for ReactionError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            ReactionError::IsIdentity => Display::fmt("collision result is identity function", f),
-            ReactionError::IsParent => Display::fmt("collision result is isomorphic to parent", f),
-            ReactionError::ExceedsReductionLimit => {
-                Display::fmt("collision exceeds reduction limit", f)
-            }
-            ReactionError::NotEnoughExpressions => {
-                Display::fmt("not enough expressions for further reactions", f)
-            }
-            ReactionError::HasFreeVariables => {
-                Display::fmt("collision result has free variables", f)
-            }
-
-            ReactionError::ExceedsDepthLimit => {
-                Display::fmt("expression exceeds depth limit during reduction", f)
-            }
-        }
-    }
-}
-
-impl std::error::Error for ReactionError {}
