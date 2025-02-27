998710a97bd95d0dd71822a786339aa9d54b6dcc
agentelement <agentelement@protonmail.com>
2025-02-27 13:04:02 -0700

    build: remove unnecessary dependencies

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       {
         defaultPackage = naersk-lib.buildPackage ./.;
         devShell = with pkgs; mkShell {
-          buildInputs = [cargo-flamegraph jq pkg-config fontconfig cargo rustc rustfmt pre-commit rustPackages.clippy ];
+          buildInputs = [cargo-flamegraph jq cargo rustc rustfmt pre-commit rustPackages.clippy ];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
         };
       }
