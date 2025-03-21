1074899b0eb2aa8880088bbfea098516457dc94b
agentelement <agentelement@protonmail.com>
2025-03-21 11:31:10 -0700

    feat: lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+/// Simulation analysis
+pub mod analysis;
+
+/// Global configuration
+pub mod config;
+
+/// Random expression generators
+pub mod generators;
+
+/// Main AlChemy simulation module
+pub mod supercollider;
+
+/// Experimental stuff
+pub mod experiments;
+
+/// Utilities
+pub mod utils;
+
+/// Lambda-calculus stuff
+pub mod lambda;
