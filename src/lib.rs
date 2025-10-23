6b05cfed28d080c4a5f65434dd75692a0368a890
Cole Mathis <cole.mathis.ool@gmail.com>
2025-10-23 14:40:56 -0700

    Updating lib file, should not have Cli in it

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,13 @@
 use pyo3::prelude::*;
 
 // Re-export your Rust modules for the CLI and for external users
+pub mod analysis;
 pub mod config;
 pub mod experiments;
 pub mod generators;
-pub mod utils;
-pub mod analysis;
 pub mod lambda;
 pub mod supercollider;
+pub mod utils;
 
 // New Python wrapper module
 mod python;
@@ -16,19 +16,5 @@ mod python;
 #[pymodule]
 fn alchemy(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
     python::register(m)
-}
-
-// If you still need this for the CLI, keep it here:
-use lambda_calculus::{parse, term::Notation::Classic, Term};
-use std::io::{self, BufRead};
-
-
-
-pub fn read_inputs() -> impl Iterator<Item = Term> {
-    io::stdin()
-        .lock()
-        .lines()
-        .filter_map(|line| line.ok())
-        .filter_map(|line| parse(&line, Classic).ok())
 }
 
